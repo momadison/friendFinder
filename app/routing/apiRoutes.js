2d1a444013474b7ab7cd10ce520561c8a93336fc
momadison <mattomadison.gmail.com>
2019-05-02 11:33:02 -0500

    updating routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,8 @@
+// var express = require(express);
+// var app = express();
+
+// // Routes
+// app.get("/api/friends", function(req, res) {
+//     return res.json(friends);
+// });
+
--- server.js
+++ server.js
@@ -1,34 +1,75 @@
 // Dependencies
+// =============================================================
 var express = require("express");
 var path = require("path");
-// var exphbs = require("express-handlebars");
 
-// Create an instance of the express app.
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = 3000;
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Set Handlebars as the default templating engine.
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// Data
-
+// Star Wars Characters (DATA)
+// =============================================================
+var friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+];
+ 
 
 // Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
+// Displays all characters
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
 
-// Start our server so that it can begin listening to client requests.
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newcharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("App listening on PORT " + PORT);
 });
