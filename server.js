c0aa23e2f056d650eee1139db6641174018b9084
momadison <mattomadison.gmail.com>
2019-05-02 18:08:35 -0500

    final update -- hopefully - modals working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3101;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -46,26 +46,35 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
-// Displays all characters
+// Displays all friends
 app.get("/api/friends", function(req, res) {
   return res.json(friends);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newcharacter = req.body;
+app.post("/api/friends", function(req, res) {
+    var seenDuplicate = false;
+    var score = req.body.scores.reduce(getSum, 0);
+    var difference = 50;
+    var bestFriend = {};
+    
+    for (var i = 0; i < friends.length; i++) {
+        if (friends[i].name === req.body.name) {
+           seenDuplicate = true
+        }
+        if ((Math.abs(friends[i].scores.reduce(getSum, 0) - score)) < difference) {
+            bestFriend = friends[i];
+            difference = Math.abs(friends[i].scores.reduce(getSum, 0) - score)
+        }
+    }
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
+    if (seenDuplicate === false) {
+        console.log("pushing");
+        friends.push(req.body);
+    } else {
+        console.log("You have already taken this survey");
+    }
+    
+    res.json(bestFriend);
 });
 
 // Starts the server to begin listening
@@ -73,3 +82,7 @@ app.post("/api/characters", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+function getSum(total, num) {
+    return total + Math.round(num);
+}
